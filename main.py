cec2160501f837bb4fec9cdb8c1a3fd5401f3f1c
Uhpachee <sea7wa@gmail.com>
2021-03-26 17:15:03 -0700

    added routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, Blueprint, render_template
 from people import people_bp
 from people.prep import people_prep_bp
 from people.David import people_David_bp
@@ -20,6 +20,17 @@ app.register_blueprint(people_Gavin_bp, url_prefix='/people/Gavin')
 def index():
     return render_template("index.html")
 
+@app.route('/itunes')
+def index():
+    return render_template("itunes.html")
+
+@app.route('/soundcloud')
+def index():
+    return render_template("soundcloud.html")
+
+@app.route('spotify')
+def index():
+    return render_template("spotify.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
