c9cee090d2ae942b3ce166845183175c6c95ad04
David Kim <davidhkim1102@gmail.com>
2021-04-02 18:38:54 -0700

    minilab update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from people.David import people_David_bp
 from people.Brandon import people_Brandon_bp
 from people.Kian import people_Kian_bp
 from people.Gavin import people_Gavin_bp
-
+# from people.David.david_minilab import ChessPiece
 
 app = Flask(__name__)
 app.register_blueprint(people_bp, url_prefix='/people/repos')
@@ -21,21 +21,37 @@ def index():
     return render_template("index.html")
 
 @app.route('/itunes')
-def index():
+def itunes():
     return render_template("itunes.html")
 
 @app.route('/soundcloud')
-def index():
+def soundcloud():
     return render_template("soundcloud.html")
 
-@app.route('spotify')
-def index():
+@app.route('/spotify')
+def spotify():
     return render_template("spotify.html")
+
+@app.route('/minilab/david')
+def david_minilab():
+    piece = "WB2"
+    board = {"a8": "  ", "b8": "BR1n", "c8": "BB1", "d8": "BQ1", "e8": "BK1n", "f8": "BB2", "g8": "  ", "h8": "BR2n",
+             "a7": "bp1", "b7": "  ", "c7": "bp3", "d7": "bp4", "e7": "  ", "f7": "bp6", "g7": "bp7", "h7": "bp8",
+             "a6": "  ", "b6": "  ", "c6": "  ", "d6": "  ", "e6": "  ", "f6": "BN2", "g6": "  ", "h6": "  ",
+             "a5": "  ", "b5": "bp2", "c5": "  ", "d5": "WB2", "e5": "bp5", "f5": "  ", "g5": "  ", "h5": "  ",
+             "a4": "  ", "b4": "BN1", "c4": "  ", "d4": "  ", "e4": "wp5", "f4": "  ", "g4": "  ", "h4": "  ",
+             "a3": "  ", "b3": "  ", "c3": "  ", "d3": "wp4", "e3": "  ", "f3": "WN2", "g3": "  ", "h3": "  ",
+             "a2": "wp1", "b2": "wp2", "c2": "wp3", "d2": "  ", "e2": "  ", "f2": "wp6", "g2": "wp7", "h2": "wp8",
+             "a1": "WR1n", "b1": "WN1", "c1": "WB1", "d1": "WQ1", "e1": "  ", "f1": "WR2", "g1": "WK1", "h1": "  "}
+
+    # chesspiece = ChessPiece(board, piece)
+    return render_template("david-minilab.html", )
 
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
 
 
 if __name__ == "__main__":
