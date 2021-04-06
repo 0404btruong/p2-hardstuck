541085e204c3bebb38d5bc541567a7c9a9b25cb1
gavin <gavintheriault10@gmail.com>
2021-04-06 10:14:49 -0700

    new mini lab

--- people/Gavin/gavinsminilab.py
+++ people/Gavin/gavinsminilab.py
@@ -1,20 +1,24 @@
+from random import random
+
+from flask import render_template
+superherolist1= ["Superman", "Batman", "Ironman"]
 class Superhero:
-    def __init__(self, series):
-        if series < 0 or series > 10:
-            raise ValueError("Must be between 0 and 10")
-        self._series = series
+    def __init__(self, number):
+        if number < 0 or number > 3:
+            raise ValueError("Must be between 0 and 3")
+        self._number = number
         self._list = []
         self._dict = {}
         self._dictID = 0
-        self.superheo_series()
+        self.superhero_series()
 
     def superhero_series(self):
-        limit = self._series
-        f = [(random.sample((superherolist1), k=3))]
-        while limit > 0:
-            self.set_data(f[0])
-            f = [f[0]]
-            limit -= 1
+
+        f = [(random((superherolist1), k=self._number))]
+
+        self.set_data(f[0])
+        f = [f[0]]
+
 
     def set_data(self, num):
         self._list.append(num)
@@ -37,13 +41,9 @@ class Superhero:
         return self._dict[nth]
 
 if __name__ == "__main__":
-    x = 7
-    superherobest = superhero(x/x)
+    x = 2
+    superherobest = Superhero(x)
     print(f"Here are some awesome superheroes! = {superherobest.list}")
 
-if request.method == 'POST' :
-    x= int(request.form.get("series"))
-    superherobest = superhero(x/x)
-    return render_template("select-superhero.html", superherobest=superheroes(int(request.form.get("series"))))
-return render_template("select-book.html", superherobest=superheroes(1))
+
 
